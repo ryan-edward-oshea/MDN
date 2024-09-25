3cc4cfe14740abea84f4ed353ef0649f4582d618
Ryan <ryan.edward.oshea@gmail.com>
2024-09-25 11:38:22 -0400

    Updated resample Rrs function

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from .__version__ import __version__
 from .product_estimation import image_estimates, get_estimates
 from .meta import get_sensor_bands
 from .utils import get_tile_data, current_support, download_example_imagery, mask_land, get_tile_geographic_info
-from .gloria_processing_utils import get_gloria_trainTestData
+from .gloria_processing_utils import get_gloria_trainTestData, resample_Rrs
 from .parameters import get_args
 from .utilities import get_mdn_preds, map_cube, get_mdn_uncert_ensemble
 from .plot_utilities import create_scatterplots_trueVsPred, display_sat_rgb, find_rgb_img, overlay_rgb_mdnProducts
